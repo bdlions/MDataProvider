953edab5550196fad83fa3c2cdec59220b241940
kabir <alamgirkabir7@gmail.com>
2015-06-16 18:33:50 +0600

    add a test file for the easy dao manipulation

--- test/com/shampan/db/DBConnecitonTest.java
+++ test/com/shampan/db/DBConnecitonTest.java
@@ -4,6 +4,12 @@
  */
 package com.shampan.db;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.model.ReturnDocument;
+import com.shampan.db.collections.UserDAO;
+import com.shampan.db.collections.fragment.Name;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -43,5 +49,33 @@ public class DBConnecitonTest {
     @Test
     public void main(){
         DBConnection.getInstance().getConnection();
+        MongoCollection<UserDAO> mongoCollection =
+                DBConnection.getInstance().getConnection().getCollection("user1", UserDAO.class);
+
+        Name n = new Name();
+        n.first = "Alamgir";
+        n.last = "Kabir";
+        UserDAO me = new UserDAO(n, "A.Kabir");
+        
+
+        mongoCollection.insertOne(me);
+
+        UserDAO meAgain = mongoCollection.find(me).first();
+//
+        System.out.println(meAgain);
+//
+
+        UserDAO improvedMe = new UserDAO(n, "A.Kabir");
+//        improvedMe.set_id(meAgain.get_id());
+//
+        FindOneAndReplaceOptions replaceOptions =
+                new FindOneAndReplaceOptions().returnDocument(ReturnDocument.AFTER);
+//
+        improvedMe = mongoCollection.findOneAndReplace(me,
+                improvedMe,
+                replaceOptions);
+//
+        System.out.println(improvedMe);
+
     }
 }
